4b582671e24f97870698d926324f631367342f0e
Alvin Cheng <ascheng21@gmail.com>
2017-08-08 16:15:20 -0700

    Installed CORS as a dependency. #1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const server = require('http').createServer(app)
@@ -9,6 +10,7 @@ const io = require('socket.io')(server, {
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.options('*', cors())
 
 io.on('connection', socket => {
   socket.on('chat-message', msg => {
