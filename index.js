01c6b66fe497d43f6a45dadc7ab8016b3c600cb0
Alvin Cheng <ascheng21@gmail.com>
2017-08-10 10:55:38 -0700

    Added route to handle POST requests to /register. #4

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const app = express()
 const server = require('http').createServer(app)
@@ -15,6 +17,17 @@ app.options('*', cors())
 io.on('connection', socket => {
   socket.on('chat-message', msg => {
     io.emit('chat-message', msg)
+  })
+})
+
+app.post('/register', (req, res) => {
+  let { username, password } = req.body
+  const payload = { username }
+  const token = jwt.sign(payload, process.env.JWT_SECRET)
+  bcrypt.hash(password, 10).then(hash => {
+    password = hash
+    // use knex to send user to datapase and return token
+    console.log(token)
   })
 })
 
